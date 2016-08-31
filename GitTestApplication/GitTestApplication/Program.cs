3ccbcbcd7603afcfaf5531c9374ef101b9af82b3
jenyap <jenyap@marvell.com>
2016-08-31 10:57:45 +0300

    Add new project

---
+++ GitTestApplication/GitTestApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing GIT");
+            Console.ReadKey();
+        }
+    }
+}
